b48405a918a1f793ae59b59e6a1fae2bf0b5a604
hellotksan <tk0110@au.com>
2024-10-29 22:03:17 +0900

    gitignore .env.test

--- app/app.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import usersHandler from "@/app/api/users"; // あなたのユーザーAPIハンドラのパス
-
-export default function app(req: NextApiRequest, res: NextApiResponse) {
-  return usersHandler(req, res);
-}
--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1", // エイリアスの設定を調整
-  },
-};
